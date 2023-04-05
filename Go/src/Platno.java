c1a600c355b3192f95520bd055d27602f6557e4f
Milka Gruber <gruber.milka@gmail.com>
2023-04-05 10:02:47 +0200

    Spremenjeno Platno

--- Go/src/Platno.java
+++ Go/src/Platno.java
@@ -1,4 +1,92 @@
+import java.awt.Color;
+import java.awt.Stroke;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
 
-public class Platno {
+import javax.swing.JPanel;
 
+import logika.Plosca;
+
+public class Platno extends JPanel implements MouseListener, MouseMotionListener, KeyListener {
+	protected Plosca plosca;
+	protected Color barvaMreze;
+	protected Color barvaPrviIgralec;
+	protected Color barvaDrugiIgralec;
+	protected Stroke debelinaMreze;
+	protected double polmerZetona;
+	
+	public Platno(int sirina, int visina) {
+		
+	}
+	
+	public void nastaviPlosco(Plosca p) {
+		plosca = p;
+		repaint();
+		
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e
+			) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseDragged(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseMoved(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	
 }
