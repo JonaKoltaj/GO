4b9f5fcfa40954175f3f48a1de7688ae16f969cb
Jona Koltaj <jona.koltaj@gmail.com>
2023-04-05 09:59:42 +0200

    preverjanje ce poteza obstaja

--- Go/src/logika/Igra.java
+++ Go/src/logika/Igra.java
@@ -1,11 +1,30 @@
 package logika;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import splosno.Poteza;
+
 public class Igra {
 	
 	protected Plosca plosca;
 	
 	public Igra() {
-		plosca = new Plosca(3);
+		plosca = new Plosca(9);
 	}
 	
+	public Set<Poteza> seznamMoznihPotez() {
+		Set<Poteza> seznam = new HashSet<Poteza>();
+		for (int i = 0; i < 9; ++i) {
+			for (int j = 0; j < 9; ++j) {
+				if (plosca.mreza[i][j] == null) seznam.add(new Poteza(i, j));
+			}
+		}
+		return seznam;
+	}
+	
+	public boolean odigraj(Poteza poteza) {
+		Set<Poteza> seznam = seznamMoznihPotez();
+		return seznam.contains(poteza);
+	}
 }
