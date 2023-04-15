cfafc65de70579120fb9ddf3b307bc05ea00d2cf
Milka Gruber <gruber.milka@gmail.com>
2023-04-16 01:22:10 +0200

    Dodani testi v CaptureGo za preverjanje delovanja DisjointSets

--- Go/src/CaptureGo.java
+++ Go/src/CaptureGo.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 import logika.Igra;
 import logika.Plosca;
@@ -16,15 +18,32 @@ public class CaptureGo {
 		// plosca.izpis();
 		Igra igra = new Igra();
 		igra.izpisStanja();
-		igra.odigraj(new Poteza(1,1));
+		igra.odigraj(new Poteza(1,1)); // črni
 		igra.izpisStanja();
-		igra.odigraj(new Poteza(2,2));
+		igra.odigraj(new Poteza(2,2)); // beli
 		igra.izpisStanja();
-		igra.odigraj(new Poteza(3,3));
+		igra.odigraj(new Poteza(3,3)); // črni
 		igra.izpisStanja();
-		igra.odigraj(new Poteza(3,2));
+		igra.odigraj(new Poteza(3,2)); // beli
 		igra.izpisStanja();
+		igra.odigraj(new Poteza(1,2)); // črni
+		igra.izpisStanja();
+		
+		Igra igra2 = new Igra();
+		igra2.odigraj(new Poteza(3,4));
+		igra2.izpisStanja();
+		igra2.odigraj(new Poteza(4,4));
+		igra2.izpisStanja();
+		igra2.odigraj(new Poteza(3,5));
+		igra2.izpisStanja();
+		igra2.odigraj(new Poteza(5,4));
+		igra2.izpisStanja();
+		igra2.odigraj(new Poteza(3,6));
+		igra2.izpisStanja();
+		igra2.odigraj(new Poteza(6,4));
+		igra2.izpisStanja();
 		
 	}
 
+	
 }
