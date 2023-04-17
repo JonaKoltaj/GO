e7c24db274c80f6b7a3fd7b48ae7a270be62ea81
Milka Gruber <gruber.milka@gmail.com>
2023-04-17 17:04:37 +0200

    Dokončana metoda, ki preveri ali je igre konec in drugi popravki.

--- Go/src/logika/Igra.java
+++ Go/src/logika/Igra.java
@@ -1,11 +1,10 @@
 package logika;
 
 import java.util.HashSet;
-import java.util.Map;
-import java.lang.reflect.Array;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.Set;
+
+
 
 import splosno.Poteza;
 
@@ -15,8 +14,10 @@ public class Igra {
 	protected boolean stanje; // true, če igra ni končana in false sicer
 	protected Set<Poteza> moznePoteze; // beležimo poteze, ki so na voljo
 	protected String naVrsti; // kateri izmed igralcev mora narediti potezo
-	protected DisjointSet skupineBelih; // skupina zetonov iste barve, ki tvorijo celoto
-	protected DisjointSet skupineCrnih;
+	protected DisjointSet skupineBelih; // beležimo disjunktne množice belih žetonov
+	protected DisjointSet skupineCrnih; // popravi na protected
+	protected String zmagovalec;
+	protected ArrayList<Zeton> zajetaSkupina;
 	
 
 	public Igra() {
@@ -26,6 +27,8 @@ public class Igra {
 		naVrsti = "Black";
 		skupineBelih = new DisjointSet("White");
 		skupineCrnih = new DisjointSet("Black");
+		zmagovalec = null;
+		zajetaSkupina = null;
 	}
 	
 	// metoda, ki vrne množico vseh možnih potez 
@@ -45,94 +48,96 @@ public class Igra {
 		else return "White";
 	}
 	
-	// ne vem, ali bo ta metoda sploh uporabna
+	// vrne seznam velikosti 4, ki v smeri urinega kazalca beleži sosede
+	// element z indeksom 0 je tisti s koordinatami (i + 1, j)
 	public Zeton[] vrniSosede(Zeton z) {
 		int n = plosca.velikost;
 		Zeton[] seznamSosedov = {null, null, null, null};
 		if (z == null) return seznamSosedov;
 		else {
 		if (z.i > 0 & z.i < n - 1 & z.j > 0 & z.j < n - 1) {
-			seznamSosedov[0] = plosca.mreza[z.i + 1][z.j];
+			seznamSosedov[0] = plosca.mreza[z.i - 1][z.j];
 			seznamSosedov[1] = plosca.mreza[z.i][z.j + 1];
-			seznamSosedov[2] = plosca.mreza[z.i - 1][z.j];
+			seznamSosedov[2] = plosca.mreza[z.i + 1][z.j];
 			seznamSosedov[3] = plosca.mreza[z.i][z.j - 1];
 		}
 		else if (z.i == 0 & z.j == 0) {
 			seznamSosedov[1] = plosca.mreza[z.i][z.j + 1];
-			seznamSosedov[2] = plosca.mreza[z.i - 1][z.j];
+			seznamSosedov[2] = plosca.mreza[z.i + 1][z.j];
 		}
 		else if (z.i == n - 1 & z.j == n - 1) {
-			seznamSosedov[0] = plosca.mreza[z.i + 1][z.j];
+			seznamSosedov[0] = plosca.mreza[z.i - 1][z.j];
 			seznamSosedov[3] = plosca.mreza[z.i][z.j - 1];
 		}
 		else if (z.i == 0 & z.j == n - 1) {
-			seznamSosedov[2] = plosca.mreza[z.i - 1][z.j];
+			seznamSosedov[2] = plosca.mreza[z.i + 1][z.j];
 			seznamSosedov[3] = plosca.mreza[z.i][z.j - 1];
 		}
 		else if (z.i == n - 1 & z.j == 0) {
-			seznamSosedov[0] = plosca.mreza[z.i + 1][z.j];
+			seznamSosedov[0] = plosca.mreza[z.i - 1][z.j];
 			seznamSosedov[1] = plosca.mreza[z.i][z.j + 1];
 		}
 		else if (z.i == 0 & z.j != 0 & z.j != n - 1) {
 			seznamSosedov[1] = plosca.mreza[z.i][z.j + 1];
-			seznamSosedov[2] = plosca.mreza[z.i - 1][z.j];
+			seznamSosedov[2] = plosca.mreza[z.i + 1][z.j];
 			seznamSosedov[3] = plosca.mreza[z.i][z.j - 1];
 		} 
 		else if (z.i == n - 1 & z.j != 0 & z.j != n - 1) {
-			seznamSosedov[0] = plosca.mreza[z.i + 1][z.j];
+			seznamSosedov[0] = plosca.mreza[z.i - 1][z.j];
 			seznamSosedov[1] = plosca.mreza[z.i][z.j + 1];
 			seznamSosedov[3] = plosca.mreza[z.i][z.j - 1];
 		}
 		else if (z.j == 0 & z.i != 0 & z.i != n - 1) {
-			seznamSosedov[0] = plosca.mreza[z.i + 1][z.j];
+			seznamSosedov[0] = plosca.mreza[z.i - 1][z.j];
 			seznamSosedov[1] = plosca.mreza[z.i][z.j + 1];
-			seznamSosedov[2] = plosca.mreza[z.i - 1][z.j];
+			seznamSosedov[2] = plosca.mreza[z.i + 1][z.j];
 		}
 		else if (z.j == n - 1 & z.i != 0 & z.i != n - 1) {
-			seznamSosedov[0] = plosca.mreza[z.i + 1][z.j];
-			seznamSosedov[2] = plosca.mreza[z.i - 1][z.j];
+			seznamSosedov[0] = plosca.mreza[z.i - 1][z.j];
+			seznamSosedov[2] = plosca.mreza[z.i + 1][z.j];
 			seznamSosedov[3] = plosca.mreza[z.i][z.j - 1];
 		}
 		return seznamSosedov;
 		}
 	}
 	
-	// z uporabo razreda DisjoinSet, žeton z priključimo skupini svoje barve
-	// tukaj predpostavimo, da ta korak naredimo natanko enkrat, torej ne more se zgoditi, da bi žeton že bil v množici
+	
+	// z uporabo razreda DisjointSet, ustavimo enojec in naredimo unijo s sosedi iste barve
 	public void prikljuciSkupini(Zeton z) {
 		Zeton[] sosedi = vrniSosede(z);
 		boolean aliImaSoseda = false;
 		for (Zeton s : sosedi) {if (s != null) { if (s.barva == z.barva) aliImaSoseda = true; }
 		}
-		
-		// če nima soseda iste barve, bomo uporabili makeSet in ustvarili enojec
+		// če nima nobenega soseda iste barve, bomo samo ustvarili enojec
 		if (aliImaSoseda == false) {
-			if (z.barva == "White") skupineBelih.makeSet(z);
-			else skupineCrnih.makeSet(z);
-			}
-		
-		// sicer najdemo prvega soseda iste barve in naredimo unijo
-		else {
-			int i = 0;
-			boolean nadaljuj = true;
-			while (nadaljuj & i != 4) {
-				if (sosedi[i] != null) {
-					// če se barvi ujemata
-					if (sosedi[i].barva == z.barva) {
-						if (z.barva == "White") {skupineBelih.makeSet(z); skupineBelih.union(z, sosedi[i]);}
-						else {skupineCrnih.makeSet(z); skupineCrnih.union(z, sosedi[i]);}
-						nadaljuj = false;
-						}
-					
-					// če se barvi ne ujemata, gremo naprej
-					else ++i;
+			if (z.barva == "White") {
+				skupineBelih.makeSet(z); 
+				skupineBelih.predstavniki.add(z);
 				}
-				else ++i;
+			else {
+				skupineCrnih.makeSet(z); 
+				skupineCrnih.predstavniki.add(z);
+				}
+			}
+		// sicer naredimo unijo z vsemi sosedi iste barve
+		else {
+			for (Zeton s : sosedi) {
+				if (s != null) { // preskočimo prazna polja
+					if (s.barva == z.barva) {  
+						if (z.barva == "White") {
+							skupineBelih.makeSet(z); 
+							skupineBelih.union(z, s);
+							}
+						else {
+							skupineCrnih.makeSet(z); 
+							skupineCrnih.union(z, s);
+						}
+					}
+				}
 			}
 		}
 	}
 		
-	
 	
 	
 	// metoda, ki vzame željeno potezo, jo odigra, če je to mogoče, ter vrne true,
@@ -142,10 +147,8 @@ public class Igra {
 		int j = poteza.y();
 		if (moznePoteze.contains(poteza)) {
 			Zeton novZeton = new Zeton(i, j, naVrsti);
+			System.out.println("Barva " + naVrsti + " na mesto: " +  "(" + novZeton.i + "," + novZeton.j + ")");
 			plosca.postaviZeton(novZeton);
-			// ko postavimo žeton, preverimo njegove sosede. Če je na meji s kakšnim, ga damo v pripadajočo skupino,
-			// sicer ustvarimo novo skupino
-			// to naredi pomožna metoda prikljuciSkupini
 			prikljuciSkupini(novZeton);
 			moznePoteze.remove(poteza);
 			naVrsti = drugi(naVrsti);
@@ -154,25 +157,69 @@ public class Igra {
 		else return false;
 	}
 	
-	// metoda za izpis stanja igre - pomembna samo med testiranjem, kasneje izbrišem
-	public  void izpisStanja() {
-		if (stanje == false) {System.out.println("Igre je konec. Zmagal je: " + drugi(naVrsti));
-		plosca.izpis();}
-		else {
-		System.out.println("Na vrsti je igralec: " + naVrsti);
-		plosca.izpis();
-		System.out.println("Disjunktne unije belih so");
-		skupineBelih.izpisStarsev();
-		skupineBelih.izpisRanka();
-		System.out.println("Disjunktne unije črnih so");
-		skupineCrnih.izpisStarsev();
-		skupineCrnih.izpisRanka();
-		}
-	}
 	
+	
+	// metoda, ki sprejme seznam skupine žetonov in vrne prazen rob
+	// t.j. mesta, ki so še prosta na robu skupine
+	public  HashSet<Par> vrniProsta(ArrayList<Zeton> seznam) {
+		HashSet<Par> sez = new HashSet<Par>();
+		int n = plosca.velikost;
+			for (Zeton z : seznam) {
+				Zeton[] sosediZetona = vrniSosede(z);
+				if (sosediZetona[0] == null & z.i - 1 >= 0) sez.add(new Par(z.i - 1, z.j));
+				if (sosediZetona[1] == null & z.j + 1 < n) sez.add(new Par(z.i, z.j + 1));
+				if (sosediZetona[2] == null & z.i + 1 < n) sez.add(new Par(z.i + 1, z.j));
+				if (sosediZetona[3] == null & z.j - 1 > 0) sez.add(new Par(z.i, z.j - 1));
+				}
+			return sez;
+	}
+
 	
 	// metoda, ki preveri ali je igre konec
-	public static void aliJeKonec() {
-		// metoda se bo sprehodila po "robu" od vsake skupine in preverila ali je skupina ujeta
+	// šli bomo po vseh predstavnikih in pogledali, ali imajo vse skupine še kakšno prosto mesto
+	// če kakšna skupina nima prostega roba, to pomeni, da jo je nasprotnik zajel, torej je igre konec
+	public boolean aliJeKonec() {
+		boolean igraVTeku = true;
+		for (Zeton z: skupineBelih.predstavniki) {
+			HashSet<Par> sosedi = vrniProsta(skupineBelih.vrniSkupino(z));
+			if (sosedi.isEmpty()) {
+				igraVTeku = false;
+				zmagovalec = drugi(z.barva);
+				zajetaSkupina = skupineBelih.vrniSkupino(z);
+				stanje = false;}
+		}
+		for (Zeton z: skupineCrnih.predstavniki) {
+			HashSet<Par> sosedi = vrniProsta(skupineCrnih.vrniSkupino(z));
+			if (sosedi.isEmpty()) {igraVTeku = false;
+			zmagovalec = drugi(z.barva);
+			zajetaSkupina = skupineCrnih.vrniSkupino(z);
+			stanje = false;}
+		}
+		if (igraVTeku == false) konecIgre(); // samo izpis, kasneje izbrišem
+		return igraVTeku;
 	}
+	
+	
+	
+		// metoda za izpis stanja igre - pomembna samo med testiranjem, kasneje izbrišem
+		public  void izpisStanja() {
+			plosca.izpis();
+			System.out.println("********************************************************************");
+			aliJeKonec();
+			System.out.println("********************************************************************");
+		}
+	
+		// pomožna metoda za izpis rezultata, kasneje izbrišem
+		public static void sprintaj(ArrayList<Zeton> sez) {
+			for (Zeton z : sez) System.out.print(" " + z.barva + "(" + z.i+ " , " + z.j + ") ");
+			System.out.println("");
+		}
+		
+		// metoda, ki izpiše rezultat igre, kasneje izbrišem
+		public void konecIgre() {
+			System.out.println("IGRE JE KONEC!");
+			System.out.println("ZMAGAL JE IGRALEC BARVE: " + zmagovalec);
+			System.out.println("ZAJEL JE SKUPINO: ");
+			sprintaj(zajetaSkupina);
+		}
 }
